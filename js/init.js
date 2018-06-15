6f573db7d730daf3fd6fa2a7219944c96e0fbc3e
narangricha <richa8feb1995@gmail.com>
2018-06-15 11:11:42 +0530

    Pics Update 2

--- js/init.js
+++ js/init.js
@@ -5,4 +5,10 @@
     $('.parallax').parallax();
 
   }); // end of document ready
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: false
+  });
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
 })(jQuery); // end of jQuery name space
